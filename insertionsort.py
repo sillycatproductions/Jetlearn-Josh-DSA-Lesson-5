8920af704058575fd8b826f6a6a07c343b743ac9
sillycatproductions <156546562+sillycatproductions@users.noreply.github.com>
2024-09-21 11:21:41 +0100

    Add files via upload

---
+++ insertionsort.py
@@ -0,0 +1,14 @@
+a = [5,3,6,37,5455,63546,546,456,56,7]
+def InsertionSort(a):
+    for i in range(1, len(a)):
+        b = a[i]
+        c = i - 1
+
+        while c >= 0 and b < a[c]:
+            a[c+1] = a[c]
+            c -= 1
+        a[c+1] = b
+    
+print('Unsorted: ', a)
+InsertionSort(a)
+print('Sorted: ', a)
